edd694f584a18cef637b5780e033b1c0a131639e
aline <aline.ramos@gocase.com.br>
2021-03-23 21:00:35 -0300

    order details

--- app/models/command.rb
+++ app/models/command.rb
@@ -7,4 +7,8 @@ class Command < ApplicationRecord
   enum status: [:open, :paid]
   
   scope :from_order, -> (order_id){ where(order_id: order_id) }  
+
+  def total
+    line_items.count > 0  ? line_items.map { |li| (li.price*li.quantity) }.inject(:+) : 0.0
+  end
 end
