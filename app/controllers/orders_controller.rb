b0bcbfcf3386f49ec14dd14bfb40bf1338271790
aline <aline.ramos@gocase.com.br>
2021-04-10 14:11:57 -0300

    products

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   # GET /orders or /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.paginate(page: params[:page], per_page: 15)
   end
 
   # GET /orders/1 or /orders/1.json
