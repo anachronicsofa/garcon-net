d73b2d173887307f498598b5b6cd579c2fd16d84
aline <aline.ramos@gocase.com.br>
2021-12-02 16:37:59 -0300

    New tables qr code

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,3 +1,5 @@
+require "rqrcode"
+
 class Table < ApplicationRecord
   has_many :orders
   validates :reference, :status, presence: true 
@@ -5,4 +7,16 @@ class Table < ApplicationRecord
   enum status: [:available, :reserved, :open]
 
   scope :ordered_by_ref, -> { order(reference: :asc) }
+
+  def qr_code
+    qrcode = RQRCode::QRCode.new("http://garcon-net.herokuapp.com/tables/" + id.to_s)
+    svg = qrcode.as_svg(
+      color: "000",
+      shape_rendering: "crispEdges",
+      module_size: 5,
+      standalone: true,
+      use_path: true
+    )
+    svg.html_safe
+  end
 end
