0f467f34e507aaa6a940d64055997c626da226ae
aline <aline.ramos@gocase.com.br>
2021-12-09 22:23:03 -0300

    fix command json

--- app/controllers/api/tables_controller.rb
+++ app/controllers/api/tables_controller.rb
@@ -1,25 +1,28 @@
 class Api::TablesController < ApplicationController
   respond_to :json
-    
+
   def items
     order = Order.where(status: 'open', table_id: params[:table_id]).first
     if order
       response = {
-        commands: order.commands.map do |command|
+        order: orders.map do |order|
           {
-            client_name: command.client_name,
-            command_id: command.id,
-            total: command.total,
-            status: command.status,
-            line_items: command.line_items.map do |li| 
+            commands: order.commands.map do |command|
               {
-                id: li.id,
-                quantity: li.quantity,
-                status: li.status,
-                price: li.price,
-                command_id: li.command_id,
-                product: li.product,
-                time_to_prepare: li.time_to_prepare
+                client_name: command.client_name,
+                command_id: command.id,
+                total: command.total,
+                status: command.status,
+                line_items: command.line_items.map do |li| 
+                  {
+                    id: li.id,
+                    quantity: li.quantity,
+                    status: li.status,
+                    price: li.price,
+                    command_id: li.command_id,
+                    product: li.product
+                  }
+                end
               }
             end
           }
